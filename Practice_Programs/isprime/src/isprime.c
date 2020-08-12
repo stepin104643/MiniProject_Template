b2c4e8e3d81dc54b7513d9053f0712c9cdd2ebec
Nazu <hadnazafu@Nazus-MacBook-Pro.local>
2020-08-12 20:19:11 +0530

    added documentation directory

--- Practice_Programs/isprime/src/isprime.c
+++ Practice_Programs/isprime/src/isprime.c
@@ -1,11 +1,19 @@
+///@file isprime.c
 #include <stdio.h>
 #include <stdlib.h>
 #include "isprime.h"
+
+/**
+* Checks if given number is prime or not
+* @param[in] n
+* @param[out] 0 or 1 (boolean False or True) 
+*/
+
 int checkint(int n){
 
 for(int i=2;i<n;i++){
 
-//if divisible by any number other than 1 and itself
+/* if divisible by any number other than 1 and itself */
 
 if(n%i==0){
 
@@ -16,6 +24,7 @@ return 0;
 
 }
 
+/* else */
 return 1;
 
 }
